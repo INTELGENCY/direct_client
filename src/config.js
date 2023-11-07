dc31ffde85b05d3733ab7d3b7009be7244241851
Abdullah Asif <chaudharyabdullahasif@gmail.com>
2023-11-07 16:41:01 +0500

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const keys = {
-  // api: "http://localhost:5000/",  
-  api: "https://direct.adaptable.app/", 
+  // api: "http://localhost:5000/",
+  // api: "https://direct.adaptable.app/",
+  api: "https://directserver.adaptable.app/",
   // api: "https://badger-primary-muskox.ngrok-free.app/",
 };
