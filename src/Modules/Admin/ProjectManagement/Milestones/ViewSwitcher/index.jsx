447b338fc16d86354e386b1bff1bf09fd79c5820
Abdullah Asif <chaudharyabdullahasif@gmail.com>
2023-11-13 11:49:48 +0500

    gant chart updated

--- src/Modules/Admin/ProjectManagement/Milestones/ViewSwitcher/index.jsx
+++ src/Modules/Admin/ProjectManagement/Milestones/ViewSwitcher/index.jsx
@@ -1,39 +1,19 @@
-import React from "react";
 import "gantt-task-react/dist/index.css";
-import React from "react";
-import React from "react";
+import { Button, ButtonGroup, Stack } from "@mui/material";
 
 function ViewSwitcher({ onViewModeChange, onViewListChange, isChecked }) {
   return (
-    <div className="ViewContainer">
-      <button className="Button" onClick={() => onViewModeChange("QuarterDay")}>
-        Quarter of Day
-      </button>
-      <button className="Button" onClick={() => onViewModeChange("HalfDay")}>
-        Half of Day
-      </button>
-      <button className="Button" onClick={() => onViewModeChange("Day")}>
-        Day
-      </button>
-      <button className="Button" onClick={() => onViewModeChange("Week")}>
-        Week
-      </button>
-      <button className="Button" onClick={() => onViewModeChange("Month")}>
-        Month
-      </button>
-
-      <div className="Switch">
-        <label className="Switch_Toggle">
-          <input
-            type="checkbox"
-            defaultChecked={isChecked}
-            onClick={() => onViewListChange(!isChecked)}
-          />
-          <span className="Slider" />
-        </label>
-        Show Task List
-      </div>
-    </div>
+    <Stack direction={"row"} justifyContent={"flex-end"}>
+      <ButtonGroup
+        color="success"
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button onClick={() => onViewModeChange("Day")}>Today</Button>
+        <Button onClick={() => onViewModeChange("Week")}>Week</Button>
+        <Button onClick={() => onViewModeChange("Month")}>Month</Button>
+      </ButtonGroup>
+    </Stack>
   );
 }
 
